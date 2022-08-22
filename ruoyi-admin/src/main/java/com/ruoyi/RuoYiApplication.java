daa74cd21cc456e819152beb3c05dada48aa41be
yuanzigsa <yuanzigsa@gmail.com>
2022-08-22 14:24:40 +0800

    启动

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -10,10 +10,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
-    public static void main(String[] args)
-    {
+public class RuoYiApplication {
+    public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
